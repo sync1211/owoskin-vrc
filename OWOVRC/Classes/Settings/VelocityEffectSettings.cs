b9c23a941f147f6e1c3d5bcd1cf67ce736888703
Sync1211 <dpf12110@gmail.com>
2024-11-20 01:24:18 +0100

    * Adjusted default speed limit

--- OWOVRC/Classes/Settings/VelocityEffectSettings.cs
+++ OWOVRC/Classes/Settings/VelocityEffectSettings.cs
@@ -6,7 +6,7 @@
         public bool ImpactEnabled { get; set; } = true;
         public int StopPriority { get; set; } = 1;
         public float StopVelocityThreshold { get; set; } = 10;
-        public float SpeedCap { get; set; } = 200.0f;
+        public float SpeedCap { get; set; } = 50.0f;
         public bool IgnoreWhenGrounded { get; set; }
         public bool IgnoreWhenSeated { get; set; }
         public TimeSpan StopVelocityTime { get; set; } = TimeSpan.FromSeconds(1);
