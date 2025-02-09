547fdca1d56344ce2756284de1a9ef63f4abfb72
Sync1211 <dpf12110@gmail.com>
2025-02-09 23:26:24 +0100

    Added back muscles to default audio effect muscles

--- OWOVRC/Classes/Settings/AudioEffectSettings.Defaults.cs
+++ OWOVRC/Classes/Settings/AudioEffectSettings.Defaults.cs
@@ -8,17 +8,23 @@ namespace OWOVRC.Classes.Settings
         [JsonIgnore]
         private static readonly Muscle[] DefaultBassMuscles = [
             Muscle.Pectoral_R,
-            Muscle.Pectoral_L
+            Muscle.Pectoral_L,
+            Muscle.Dorsal_R,
+            Muscle.Dorsal_L
         ];
         [JsonIgnore]
         private static readonly Muscle[] DefaultSubBassMuscles = [
             Muscle.Abdominal_R,
-            Muscle.Abdominal_L
+            Muscle.Abdominal_L,
+            Muscle.Lumbar_R,
+            Muscle.Lumbar_L
         ];
         [JsonIgnore]
         private static readonly Muscle[] DefaultTrebleMuscles = [
             Muscle.Pectoral_R,
             Muscle.Pectoral_L,
+            Muscle.Dorsal_R,
+            Muscle.Dorsal_L,
             Muscle.Arm_R,
             Muscle.Arm_L
         ];
