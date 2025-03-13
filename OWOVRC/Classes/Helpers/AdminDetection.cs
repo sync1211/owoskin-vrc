ae148eeddfe64348679cceb73194280a24a9b77d
Sync1211 <dpf12110@gmail.com>
2025-03-13 20:41:16 +0100

    Implemented detection for WINE Fixed admin warning appearing when running via WINE

--- OWOVRC/Classes/Helpers/AdminDetection.cs
+++ OWOVRC/Classes/Helpers/AdminDetection.cs
@@ -1,4 +1,6 @@
-﻿using System.Security.Principal;
+﻿using Microsoft.Win32;
+using Serilog;
+using System.Security.Principal;
 
 namespace OWOVRC.Classes.Helpers
 {
@@ -6,9 +8,19 @@ namespace OWOVRC.Classes.Helpers
     {
         public static bool IsRunningAsAdmin()
         {
+            if (IsRunningViaWine())
+            {
+                Log.Information("OWOVRC is running via WINE. Nice!");
+                return false; // Using wine will cause the program to run as admin in the context of wine
+            }
             WindowsIdentity identity = WindowsIdentity.GetCurrent();
             WindowsPrincipal principal = new(identity);
             return principal.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+
+        public static bool IsRunningViaWine()
+        {
+            return Registry.LocalMachine.OpenSubKey("Software\\Wine", false) != null;
         }
     }
 }
