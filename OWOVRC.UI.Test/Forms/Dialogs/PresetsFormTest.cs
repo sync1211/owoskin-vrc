850417f6c0c0c8c3d6d3fccd3d3b62a552700e77
Sync1211 <dpf12110@gmail.com>
2025-06-10 21:08:29 +0200

    Added tests for GetNonCollidingName and ScalingHelper

---
+++ OWOVRC.UI.Test/Forms/Dialogs/PresetsFormTest.cs
@@ -0,0 +1,75 @@
+﻿using OWOGame;
+using OWOVRC.Classes.Effects.OSCPresets;
+using OWOVRC.UI.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OWOVRC.UI.Test.Forms.Dialogs
+{
+    [TestClass]
+    public class PresetsFormTest
+    {
+        [TestMethod]
+        public void TestGetNonCollidingNameNone()
+        {
+            const string name = "Testo";
+            const string expected = "Testo (1)";
+
+            string sensationStringDummy = Sensation.Ball.ToString();
+            List<OSCSensationPreset> presets =
+            [
+                new OSCSensationPreset(true, "Test", 1, 100, false, false, sensationStringDummy),
+                new OSCSensationPreset(true, "Test (1)", 1, 100, false, false, sensationStringDummy),
+                new OSCSensationPreset(true, "Test (3)", 1, 100, false, false, sensationStringDummy),
+                new OSCSensationPreset(true, "Test (4)", 1, 100, false, false, sensationStringDummy)
+            ];
+
+            string result = PresetsForm.GetNonCollidingName(name, presets, StringComparison.CurrentCultureIgnoreCase);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestGetNonCollidingNameSingle()
+        {
+            const string name = "Test";
+            const string expected = "Test (2)";
+
+            string sensationStringDummy = Sensation.Ball.ToString();
+            List<OSCSensationPreset> presets =
+            [
+                new OSCSensationPreset(true, "Test", 1, 100, false, false, sensationStringDummy),
+                new OSCSensationPreset(true, "Test (1)", 1, 100, false, false, sensationStringDummy),
+                new OSCSensationPreset(true, "Test (3)", 1, 100, false, false, sensationStringDummy),
+                new OSCSensationPreset(true, "Test (4)", 1, 100, false, false, sensationStringDummy)
+            ];
+
+            string result = PresetsForm.GetNonCollidingName(name, presets, StringComparison.CurrentCultureIgnoreCase);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestGetNonCollidingNameMultiple()
+        {
+            const string name = "Test";
+            const string expected = "Test (3)";
+
+            string sensationStringDummy = Sensation.Ball.ToString();
+            List<OSCSensationPreset> presets =
+            [
+                new OSCSensationPreset(true, "Test", 1, 100, false, false, sensationStringDummy),
+                new OSCSensationPreset(true, "Test (1)", 1, 100, false, false, sensationStringDummy),
+                new OSCSensationPreset(true, "Test (2)", 1, 100, false, false, sensationStringDummy),
+                new OSCSensationPreset(true, "Test (4)", 1, 100, false, false, sensationStringDummy)
+            ];
+
+            string result = PresetsForm.GetNonCollidingName(name, presets, StringComparison.CurrentCultureIgnoreCase);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
