4391b674e8c4cdd6c94db577498228188230fb97
Sync1211 <dpf12110@gmail.com>
2025-08-18 18:54:15 +0200

    Removed redundant nesting

--- OWOVRC/Classes/Commandline/CommandlineSettings.cs
+++ OWOVRC/Classes/Commandline/CommandlineSettings.cs
@@ -8,30 +8,28 @@ namespace OWOVRC.Classes.Commandline
     {
         public static CommandlineArgs ParseAndApply(LoggingLevelSwitch logLevelSwitch)
         {
+            CommandlineArgs args = CommandlineParser.Parse(Environment.GetCommandLineArgs());
+
+            // CPU affinity
+            if (args.CpuAffinity != null)
             {
-                CommandlineArgs args = CommandlineParser.Parse(Environment.GetCommandLineArgs());
-
-                // CPU affinity
-                if (args.CpuAffinity != null)
-                {
-                    CPUHelper.SetCpuAffinity(args.CpuAffinity.Value);
-                }
-
-                // Process priority
-                if (args.Priority != null)
-                {
-                    CPUHelper.SetProcessPriority(args.Priority.Value);
-                }
-
-                // Log level
-                if (args.LogLevel != null)
-                {
-                    logLevelSwitch.MinimumLevel = args.LogLevel.Value;
-                    Log.Information("Log level changed to: {NewLevel}", logLevelSwitch.MinimumLevel);
-                }
-
-                return args;
+                CPUHelper.SetCpuAffinity(args.CpuAffinity.Value);
             }
+
+            // Process priority
+            if (args.Priority != null)
+            {
+                CPUHelper.SetProcessPriority(args.Priority.Value);
+            }
+
+            // Log level
+            if (args.LogLevel != null)
+            {
+                logLevelSwitch.MinimumLevel = args.LogLevel.Value;
+                Log.Information("Log level changed to: {NewLevel}", logLevelSwitch.MinimumLevel);
+            }
+
+            return args;
         }
     }
 }
