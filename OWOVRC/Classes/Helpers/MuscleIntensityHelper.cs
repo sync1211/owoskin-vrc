1591d733fb0c12233713f1cd849f3ffd3b4bdcc5
Sync1211 <dpf12110@gmail.com>
2025-10-03 23:04:41 +0200

    Fixed incorrect loop iteration count

--- OWOVRC/Classes/Helpers/MuscleIntensityHelper.cs
+++ OWOVRC/Classes/Helpers/MuscleIntensityHelper.cs
@@ -8,7 +8,7 @@ namespace OWOVRC.Classes.Helpers
         public static void AddMissingMuscles(Dictionary<int, int> muscleIntensities)
         {
             Muscle[] muscles = OWOMuscles.MuscleGroups["all"];
-            for (int i = 0; i <= muscles.Length; i++)
+            for (int i = 0; i < muscles.Length; i++)
             {
                 // Add value if the key doesn't already exist
                 muscleIntensities.TryAdd(muscles[i].id, 100);
