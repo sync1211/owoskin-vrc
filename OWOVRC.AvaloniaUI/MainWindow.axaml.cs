56914aff5d00c9d409ee25bdce824613eb88a1cb
Sync1211 <dpf12110@gmail.com>
2025-12-09 17:59:33 +0100

    Added OWOVRC.AvaloniaUI

---
+++ OWOVRC.AvaloniaUI/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace OWOVRC.AvaloniaUI
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
