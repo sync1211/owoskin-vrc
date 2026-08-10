29ec95aaf7b1d01444724798b53ffdfe82a28427
Sync1211 <dpf12110@gmail.com>
2026-08-10 13:46:45 +0200

    Small adjustments to the UI layout

---
+++ OWOVRC.UI/Forms/SpeedTestForm.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace OWOVRC.UI.Forms
+{
+    partial class SpeedTestForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SpeedTestForm));
+            playerZBar = new TrackBar();
+            playerXBar = new TrackBar();
+            sideDirectionIndicator = new OWOVRC.UI.Controls.DirectionSpeedIndicator();
+            topDirectionTitle = new Label();
+            topDirectionIndicator = new OWOVRC.UI.Controls.DirectionSpeedIndicator();
+            sideDirectionTitle = new Label();
+            playerZBar2 = new TrackBar();
+            playerYBar = new TrackBar();
+            closeButton = new Button();
+            startTestButton = new Button();
+            stopTestButton = new Button();
+            xSpeedLabel = new Label();
+            xSpeedInput = new NumericUpDown();
+            ySpeedInput = new NumericUpDown();
+            ySpeedLabel = new Label();
+            zSpeedInput = new NumericUpDown();
+            zSpeedLabel = new Label();
+            label1 = new Label();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)playerZBar).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)playerXBar).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)playerZBar2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)playerYBar).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)xSpeedInput).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)ySpeedInput).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)zSpeedInput).BeginInit();
+            SuspendLayout();
+            // 
+            // playerZBar
+            // 
+            playerZBar.LargeChange = 50;
+            resources.ApplyResources(playerZBar, "playerZBar");
+            playerZBar.Maximum = 100;
+            playerZBar.Minimum = -100;
+            playerZBar.Name = "playerZBar";
+            playerZBar.SmallChange = 10;
+            playerZBar.TickFrequency = 10;
+            playerZBar.TickStyle = TickStyle.TopLeft;
+            playerZBar.Scroll += PlayerZBar_Scroll;
+            // 
+            // playerXBar
+            // 
+            playerXBar.LargeChange = 50;
+            resources.ApplyResources(playerXBar, "playerXBar");
+            playerXBar.Maximum = 100;
+            playerXBar.Minimum = -100;
+            playerXBar.Name = "playerXBar";
+            playerXBar.SmallChange = 10;
+            playerXBar.TickFrequency = 10;
+            playerXBar.Scroll += PlayerXBar_Scroll;
+            // 
+            // sideDirectionIndicator
+            // 
+            resources.ApplyResources(sideDirectionIndicator, "sideDirectionIndicator");
+            sideDirectionIndicator.Name = "sideDirectionIndicator";
+            // 
+            // topDirectionTitle
+            // 
+            resources.ApplyResources(topDirectionTitle, "topDirectionTitle");
+            topDirectionTitle.Name = "topDirectionTitle";
+            // 
+            // topDirectionIndicator
+            // 
+            resources.ApplyResources(topDirectionIndicator, "topDirectionIndicator");
+            topDirectionIndicator.Name = "topDirectionIndicator";
+            // 
+            // sideDirectionTitle
+            // 
+            resources.ApplyResources(sideDirectionTitle, "sideDirectionTitle");
+            sideDirectionTitle.Name = "sideDirectionTitle";
+            // 
+            // playerZBar2
+            // 
+            playerZBar2.LargeChange = 50;
+            resources.ApplyResources(playerZBar2, "playerZBar2");
+            playerZBar2.Maximum = 100;
+            playerZBar2.Minimum = -100;
+            playerZBar2.Name = "playerZBar2";
+            playerZBar2.SmallChange = 10;
+            playerZBar2.TickFrequency = 10;
+            playerZBar2.Scroll += PlayerZBar2_Scroll;
+            // 
+            // playerYBar
+            // 
+            playerYBar.LargeChange = 50;
+            resources.ApplyResources(playerYBar, "playerYBar");
+            playerYBar.Maximum = 100;
+            playerYBar.Minimum = -100;
+            playerYBar.Name = "playerYBar";
+            playerYBar.SmallChange = 10;
+            playerYBar.TickFrequency = 10;
+            playerYBar.TickStyle = TickStyle.TopLeft;
+            playerYBar.Scroll += PlayerYBar_Scroll;
+            // 
+            // closeButton
+            // 
+            closeButton.DialogResult = DialogResult.Cancel;
+            resources.ApplyResources(closeButton, "closeButton");
+            closeButton.Name = "closeButton";
+            closeButton.UseVisualStyleBackColor = true;
+            closeButton.Click += CloseButton_Click;
+            // 
+            // startTestButton
+            // 
+            startTestButton.Image = Properties.Resources.Play;
+            resources.ApplyResources(startTestButton, "startTestButton");
+            startTestButton.Name = "startTestButton";
+            startTestButton.UseVisualStyleBackColor = true;
+            startTestButton.Click += StartTestButton_Click;
+            // 
+            // stopTestButton
+            // 
+            stopTestButton.Image = Properties.Resources.Pause;
+            resources.ApplyResources(stopTestButton, "stopTestButton");
+            stopTestButton.Name = "stopTestButton";
+            stopTestButton.UseVisualStyleBackColor = true;
+            stopTestButton.Click += StopTestButton_Click;
+            // 
+            // xSpeedLabel
+            // 
+            resources.ApplyResources(xSpeedLabel, "xSpeedLabel");
+            xSpeedLabel.Name = "xSpeedLabel";
+            // 
+            // xSpeedInput
+            // 
+            xSpeedInput.DecimalPlaces = 2;
+            resources.ApplyResources(xSpeedInput, "xSpeedInput");
+            xSpeedInput.Name = "xSpeedInput";
+            xSpeedInput.ValueChanged += XSpeedInput_ValueChanged;
+            // 
+            // ySpeedInput
+            // 
+            ySpeedInput.DecimalPlaces = 2;
+            resources.ApplyResources(ySpeedInput, "ySpeedInput");
+            ySpeedInput.Name = "ySpeedInput";
+            ySpeedInput.ValueChanged += YSpeedInput_ValueChanged;
+            // 
+            // ySpeedLabel
+            // 
+            resources.ApplyResources(ySpeedLabel, "ySpeedLabel");
+            ySpeedLabel.Name = "ySpeedLabel";
+            // 
+            // zSpeedInput
+            // 
+            zSpeedInput.DecimalPlaces = 2;
+            resources.ApplyResources(zSpeedInput, "zSpeedInput");
+            zSpeedInput.Name = "zSpeedInput";
+            zSpeedInput.ValueChanged += ZSpeedInput_ValueChanged;
+            // 
+            // zSpeedLabel
+            // 
+            resources.ApplyResources(zSpeedLabel, "zSpeedLabel");
+            zSpeedLabel.Name = "zSpeedLabel";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(label1, "label1");
+            label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(label2, "label2");
+            label2.Name = "label2";
+            // 
+            // SpeedTestForm
+            // 
+            resources.ApplyResources(this, "$this");
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(zSpeedInput);
+            Controls.Add(zSpeedLabel);
+            Controls.Add(ySpeedInput);
+            Controls.Add(ySpeedLabel);
+            Controls.Add(xSpeedInput);
+            Controls.Add(xSpeedLabel);
+            Controls.Add(startTestButton);
+            Controls.Add(stopTestButton);
+            Controls.Add(closeButton);
+            Controls.Add(playerZBar2);
+            Controls.Add(sideDirectionTitle);
+            Controls.Add(sideDirectionIndicator);
+            Controls.Add(topDirectionTitle);
+            Controls.Add(topDirectionIndicator);
+            Controls.Add(playerZBar);
+            Controls.Add(label2);
+            Controls.Add(playerXBar);
+            Controls.Add(label1);
+            Controls.Add(playerYBar);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "SpeedTestForm";
+            Shown += SpeedTestForm_Shown;
+            ((System.ComponentModel.ISupportInitialize)playerZBar).EndInit();
+            ((System.ComponentModel.ISupportInitialize)playerXBar).EndInit();
+            ((System.ComponentModel.ISupportInitialize)playerZBar2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)playerYBar).EndInit();
+            ((System.ComponentModel.ISupportInitialize)xSpeedInput).EndInit();
+            ((System.ComponentModel.ISupportInitialize)ySpeedInput).EndInit();
+            ((System.ComponentModel.ISupportInitialize)zSpeedInput).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TrackBar playerZBar;
+        private TrackBar playerXBar;
+        private Controls.DirectionSpeedIndicator sideDirectionIndicator;
+        private Label topDirectionTitle;
+        private Controls.DirectionSpeedIndicator topDirectionIndicator;
+        private Label sideDirectionTitle;
+        private TrackBar playerZBar2;
+        private TrackBar playerYBar;
+        private Button closeButton;
+        private Button startTestButton;
+        private Button stopTestButton;
+        private Label xSpeedLabel;
+        private NumericUpDown xSpeedInput;
+        private NumericUpDown ySpeedInput;
+        private Label ySpeedLabel;
+        private NumericUpDown zSpeedInput;
+        private Label zSpeedLabel;
+        private Label label1;
+        private Label label2;
+    }
+}
---
+++ OWOVRC.UI/Forms/SpeedTestForm.cs
@@ -0,0 +1,188 @@
+﻿using OWOVRC.Classes.Effects.Sensations;
+using OWOVRC.Classes.Effects;
+using OWOVRC.Classes.OWOSuit;
+using OWOVRC.Classes.Settings;
+
+namespace OWOVRC.UI.Forms
+{
+    public partial class SpeedTestForm : Form
+    {
+        private bool testActive;
+        private readonly OWOHelper owo;
+        private readonly WindSensation windSensation;
+        private readonly VelocityEffectSettings effectSettings;
+
+        public SpeedTestForm(OWOHelper owo, VelocityEffectSettings effectSettings)
+        {
+            InitializeComponent();
+            this.owo = owo;
+            this.effectSettings = effectSettings;
+
+            windSensation = new WindSensation(VelocityEffect.SENSATION_DURATION);
+        }
+
+        public void UpdateValues()
+        {
+            UpdateLimits();
+
+            playerXBar.Value = SpeedToPercentage((float)xSpeedInput.Value);
+            playerYBar.Value = SpeedToPercentage((float)ySpeedInput.Value);
+            playerZBar.Value = SpeedToPercentage((float)zSpeedInput.Value);
+
+
+            UpdateSensation();
+        }
+
+        private void UpdateButtonVisibility()
+        {
+            startTestButton.Visible = !testActive;
+            stopTestButton.Visible = testActive;
+        }
+
+        private void StartTestButton_Click(object sender, EventArgs e)
+        {
+            testActive = true;
+            UpdateButtonVisibility();
+
+            // Start test sensation
+            UpdateSensation();
+        }
+
+        private void StopTestButton_Click(object sender, EventArgs e)
+        {
+            testActive = false;
+            UpdateButtonVisibility();
+
+            // Stop test sensation
+            owo.StopSensation(windSensation.Name);
+        }
+
+        private void UpdateSensation()
+        {
+            //float totalSpeed = (float)speedInput.Value;
+            float totalSpeed = effectSettings.MaxSpeed;
+            float speedX = totalSpeed * (playerXBar.Value / 100f);
+            float speedY = totalSpeed * (playerYBar.Value / 100f);
+            float speedZ = totalSpeed * (playerZBar.Value / 100f);
+
+            // Top view
+            topDirectionIndicator.ValueX = speedX; // Left / Right
+            topDirectionIndicator.ValueY = speedZ; // Forward / Backward
+
+            // Side view
+            sideDirectionIndicator.ValueX = speedZ; // Forward / Backward
+            sideDirectionIndicator.ValueY = speedY; // Up / Down
+
+            // Update indicator
+            UpdateLimits();
+
+            windSensation.UpdateDirection(speedX, speedY, speedZ, effectSettings.Intensity);
+
+            if (testActive)
+            {
+                //TODO: Does not seem to be looping :[
+                windSensation.Play(owo, effectSettings.Priority);
+            }
+        }
+
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            if (testActive)
+            {
+                StopTestButton_Click(sender, e);
+            }
+
+            Close();
+        }
+
+        private void PlayerZBar_Scroll(object sender, EventArgs e)
+        {
+            playerZBar2.Value = playerZBar.Value;
+            zSpeedInput.Value = (decimal)SpeedFromPercentage(playerZBar.Value);
+
+            UpdateSensation();
+        }
+
+        private void PlayerZBar2_Scroll(object sender, EventArgs e)
+        {
+            playerZBar.Value = playerZBar2.Value;
+            zSpeedInput.Value = (decimal)SpeedFromPercentage(playerZBar.Value);
+
+            UpdateSensation();
+        }
+
+        private void PlayerXBar_Scroll(object sender, EventArgs e)
+        {
+            xSpeedInput.Value = (decimal)SpeedFromPercentage(playerXBar.Value);
+            UpdateSensation();
+        }
+
+        private void PlayerYBar_Scroll(object sender, EventArgs e)
+        {
+            ySpeedInput.Value = (decimal)SpeedFromPercentage(playerYBar.Value);
+            UpdateSensation();
+        }
+
+        private void XSpeedInput_ValueChanged(object sender, EventArgs e)
+        {
+            playerXBar.Value = SpeedToPercentage((float)xSpeedInput.Value);
+            UpdateSensation();
+        }
+
+        private void YSpeedInput_ValueChanged(object sender, EventArgs e)
+        {
+            playerYBar.Value = SpeedToPercentage((float)ySpeedInput.Value);
+            UpdateSensation();
+        }
+
+        private void ZSpeedInput_ValueChanged(object sender, EventArgs e)
+        {
+            playerZBar.Value = SpeedToPercentage((float)zSpeedInput.Value);
+            playerZBar2.Value = SpeedToPercentage((float)zSpeedInput.Value);
+            UpdateSensation();
+        }
+
+        private void UpdateLimits()
+        {
+            // Max values
+            topDirectionIndicator.MaxX = effectSettings.MaxSpeed;
+            topDirectionIndicator.MaxY = effectSettings.MaxSpeed;
+            sideDirectionIndicator.MaxX = effectSettings.MaxSpeed;
+            sideDirectionIndicator.MaxY = effectSettings.MaxSpeed;
+
+            xSpeedInput.Maximum = (decimal)effectSettings.MaxSpeed;
+            ySpeedInput.Maximum = (decimal)effectSettings.MaxSpeed;
+            zSpeedInput.Maximum = (decimal)effectSettings.MaxSpeed;
+
+            // Min values
+            xSpeedInput.Minimum = (decimal)-effectSettings.MaxSpeed;
+            ySpeedInput.Minimum = (decimal)-effectSettings.MaxSpeed;
+            zSpeedInput.Minimum = (decimal)-effectSettings.MaxSpeed;
+
+            // Threshold
+            topDirectionIndicator.ThresholdX = effectSettings.MinSpeed;
+            topDirectionIndicator.ThresholdY = effectSettings.MinSpeed;
+            sideDirectionIndicator.ThresholdX = effectSettings.MinSpeed;
+            sideDirectionIndicator.ThresholdY = effectSettings.MinSpeed;
+
+            // Force redraw
+            sideDirectionIndicator.ForceUpdate();
+            topDirectionIndicator.ForceUpdate();
+        }
+
+        private int SpeedToPercentage(float input)
+        {
+            return (int)((input / effectSettings.MaxSpeed) * 100);
+        }
+
+        private float SpeedFromPercentage(int input)
+        {
+            return input * (effectSettings.MaxSpeed / 100);
+        }
+
+        private void SpeedTestForm_Shown(object sender, EventArgs e)
+        {
+            UpdateLimits();
+        }
+    }
+}
